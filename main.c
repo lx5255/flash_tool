4363b1689e408a734fe368d0135fc36c2cadb6ef
liuxing <1043623557@qq.com>
2018-11-10 22:37:06 +0800

    基本完成文件打包成FLASH镜像文件

--- main.c
+++ main.c
@@ -7,25 +7,29 @@
 
 
 typedef unsigned char bool;
+typedef unsigned char u8;
 #define false 0
 #define true  1
 
-unsigned long ld_word(char *in)
+
+unsigned long ld_word(u8 *in)
 {
     unsigned long word;
     word = in[3]; 
     word = (word<<8)|in[2]; 
     word = (word<<8)|in[1]; 
     word = (word<<8)|in[0]; 
+    printf("word %d\n", word);
     return word;
 }
 
-void st_word(char *out, unsigned long word)
+void st_word(u8 *out, unsigned long word)
 {
-    out[0] = (word>>27)&0xff; 
-    out[1] = (word>>16)&0xff; 
-    out[2] = (word>>8)&0xff; 
-    out[3] = word&0xff; 
+    out[3] = (word>>27)&0xff; 
+    out[2] = (word>>16)&0xff; 
+    out[1] = (word>>8)&0xff; 
+    out[0] = word&0xff; 
+    printf("word %d %d %d %d\n", out[0], out[1], out[2], out[3]);
 }
 
 typedef struct 
@@ -38,15 +42,18 @@ typedef struct
 #define DPT_START           8 
 #define PART_CNT            12 
 #define FLASH_SIZE_OFFSET   16 
-bool get_flash_head(flash_inf *head, char *mbr)
+bool get_flash_head(flash_inf *head, u8 *mbr)
 {
     if((mbr ==NULL)||(head == NULL)){
         return false;
     }
     if(strcmp(FLASH_HEAD_TAG, mbr)){ 
+        printf("flash tag err\n");
         return false;
     }
+    printf("55aa %8x %8x\n", mbr[510], mbr[511]);
     if((mbr[510] != 0x55)||(mbr[511] != 0xaa)){
+        printf("55aa err\n");
         return false; 
     }
     /* memcpy(head->tag, mbr, 8); */
@@ -56,7 +63,7 @@ bool get_flash_head(flash_inf *head, char *mbr)
     return true;
 }
 
-bool st_flash_head(char *mbr, flash_inf *head)
+bool st_flash_head(u8 *mbr, flash_inf *head)
 {
     if((mbr ==NULL)||(head == NULL)){
         return false;
@@ -70,18 +77,21 @@ bool st_flash_head(char *mbr, flash_inf *head)
     return true;
 }
 
-char *get_empty_dpt(char *mbr)
+u8 *get_empty_dpt(u8 *mbr)
 {
     flash_inf head;
-    char *dpt;
+    u8 *dpt;
     if(get_flash_head(&head, mbr) == false){ 
+        printf("no flash tag\n");
         return NULL;
     }
     if(head.dpt_start > (512 - 32)){
+        printf("dpt st err %d\n", head.dpt_start);
         return NULL;
     }
     dpt = &mbr[head.dpt_start];   
     for(; dpt<&mbr[512 - 32]; dpt+=32){
+        printf("dpt %x\n", dpt);
         if((dpt[0] != 'D')||(dpt[1] != 'P') ||(dpt[2] != 'T')){
             return dpt;
         }
@@ -95,10 +105,10 @@ typedef struct
 	unsigned long part_addr;
 	unsigned long part_size;
 }dpt_inf;
-#define PART_NAME_OFFSET        4 
-#define PART_ADDR_OFFSET        16
-#define PART_SIZE_OFFSET        20 
-bool get_dpt_inf(dpt_inf *inf, char *dpt)
+#define PART_NAME_OFFSET        3 
+#define PART_ADDR_OFFSET        15
+#define PART_SIZE_OFFSET        19 
+bool get_dpt_inf(dpt_inf *inf, u8 *dpt)
 {
     if((inf == NULL)||(dpt == NULL)){
         return false;
@@ -111,7 +121,7 @@ bool get_dpt_inf(dpt_inf *inf, char *dpt)
     inf->part_size = ld_word(&dpt[PART_SIZE_OFFSET]);    
 }
 
-bool st_dpt_inf(char *dpt, dpt_inf *inf)
+bool st_dpt_inf(u8 *dpt, dpt_inf *inf)
 {
     if((inf == NULL)||(dpt == NULL)){
         return false;
@@ -119,19 +129,19 @@ bool st_dpt_inf(char *dpt, dpt_inf *inf)
     dpt[0] = 'D';
     dpt[1] = 'P';
     dpt[2] = 'T';
-    st_word(&dpt[PART_NAME_OFFSET], inf->part_name);
+    memcpy(&dpt[PART_NAME_OFFSET],inf->part_name, 12);
     st_word(&dpt[PART_ADDR_OFFSET], inf->part_addr);
     st_word(&dpt[PART_SIZE_OFFSET], inf->part_size);
 }
 
 
-bool get_outfile_name(char *out, int argc, char *argv[])
+bool get_outfile_name(u8 *out, int argc, u8 *argv[])
 {
    int arg_cnt;
    for(arg_cnt = 0; arg_cnt<argc; arg_cnt++){
        if(strcmp("-outfile", argv[arg_cnt]) == 0){ 
            if((argc - arg_cnt)>=1){
-               strcpy(out, argv[arg_cnt+0]);
+               strcpy(out, argv[arg_cnt+1]);
                return true;
            }
        }
@@ -139,7 +149,7 @@ bool get_outfile_name(char *out, int argc, char *argv[])
    return false;
 }
 
-char flash_mbr[512];
+u8 flash_mbr[512];
 int flash_size = 0;
 int copy_file(char *outfilename, char *infilename)
 {
@@ -148,9 +158,9 @@ int copy_file(char *outfilename, char *infilename)
     flash_inf flash_head;
     dpt_inf part_inf;
     bool resethead = false;
-    char *mbr = &flash_mbr;
-    char *dpt;
-    char buff[512];
+    u8 *mbr = &flash_mbr;
+    u8 *dpt;
+    u8 buff[512];
     int res = 0;
 
     if(strlen(infilename)>12){
@@ -195,7 +205,7 @@ int copy_file(char *outfilename, char *infilename)
 	    fseek(outfile,0L,SEEK_END);   /*利用fseek函数将指针定位在文件结尾的位置*/
     }
 
-    memcpy(part_inf.part_name, infilename, strlen(infilename));
+    memcpy(part_inf.part_name, infilename, strlen(infilename) == 12?12:strlen(infilename)+1);
     part_inf.part_size = 0;
     //从新写FALSH头信息
     if(resethead == true){
@@ -204,6 +214,7 @@ int copy_file(char *outfilename, char *infilename)
        flash_head.npart = 0;
        flash_head.dpt_start = 32;
        st_flash_head(mbr, &flash_head);
+       printf("resethead\n");
 	   fseek(outfile,512L,SEEK_SET); 
     } 
 
@@ -227,6 +238,10 @@ int copy_file(char *outfilename, char *infilename)
         part_inf.part_size += res;
 	}
     dpt = get_empty_dpt(mbr);  //获取一个可写的DPT
+    if(dpt == NULL){
+        res = 6;
+        goto ERR_DEAL;
+    }
     st_dpt_inf(dpt, &part_inf);
 
     flash_head.npart++;
@@ -249,7 +264,7 @@ ERR_DEAL:
 }
 
 
-char outfile_name[512];
+u8 outfile_name[512];
 bool outfile_flag = 0; 
 int main(int argc, char *argv[])
 {
@@ -275,7 +290,7 @@ int main(int argc, char *argv[])
 	res = _unlink(outfile_name);
 	printf("del file res %d", res);
 
-    char arg_cnt;
+    u8 arg_cnt;
     for(arg_cnt = 0; arg_cnt<argc; arg_cnt++){
         printf("arg:%s\n", argv[arg_cnt]);
         if(strcmp("-infile", argv[arg_cnt]) == 0){ 
